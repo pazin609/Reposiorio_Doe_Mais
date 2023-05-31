cdf493587bdc0089d3f8172228a8781417f7b495
P.azin <pedro.santos357@aluno.ce.gov.br>
2023-05-31 08:24:55 -0300

    v1
    primeira v

---
+++ Páginas JS/postagens.js
@@ -0,0 +1,5 @@
+let el = document.querySelector('.number-days');
+
+for(i = 1; 1 <= 31; i++){
+    el.innerHTML+='<span>'+i+'</span>';
+}
---
+++ Páginas JS/script.js
@@ -0,0 +1,19 @@
+
+var radio = document.querySelector('.manual-btn');
+var cont = 1;
+
+document.getElementById('radio1').checked = true;
+
+setInterval(() => {
+    proximaImg();
+}, 5000);
+
+function proximaImg(){
+    cont += 0.5;
+
+    if(cont > 4){
+        cont = 1;
+    }
+
+document.getElementById('radio'+cont).checked = true;
+}
