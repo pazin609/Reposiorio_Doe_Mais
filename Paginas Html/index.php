2857383a9c774a26aa927491402246f2bce56ae8
pazin609 <107044436+pazin609@users.noreply.github.com>
2023-06-09 13:38:16 -0300

    v21
    commit 21

--- Paginas Html/index.php
+++ Paginas Html/index.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>  
+
     <title> Agenda</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="../Páginas CSS/login.css">
-    
+
 </head>
   
       <body>
+
         <section class="area-login">
           <div class="login">
            <div>
-            <img src="">
+            <img class="logo-login" src="../Imagens/DOE MAIS.png">
            </div>
 
            <form>
@@ -22,9 +24,10 @@
               <input type="password" name="senha" placeholder="*********">
               <input type="submit" value="Entrar">
             </form>
-       <P>Ainda não tem uma conta?<a href="#">Criar conta</a></P>
+       <p class="text-cont">Ainda não tem uma conta?<a href="#">Criar conta</a></p>
 
    </div>
   </section>
+
  </body>
 </html>
